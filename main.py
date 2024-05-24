3ffe665680d5b3b132f436b97083cff0aa2bb4ab
john2ksonn <johannes.sonn@gmx.de>
2024-05-24 12:12:56 +0200

    Add missing requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import ValuePool as v
-import numpy as np
 
 __version__ = "0.1.0"
 __license__ = "Apache2"
@@ -13,8 +12,9 @@ def main():
     frunner = v.FunctionRunner(func2test)
     fuzzer = v.ValuePoolFuzzer(v.vpools, func2test)
     # print(fuzzer.fuzz())
-    lel = fuzzer.runs(frunner, max_trials=100)
-    print(lel)
+    results = fuzzer.runs(frunner, max_trials=100)
+    for result in results:
+        print(result)
 
 if __name__ == "__main__":
     main()

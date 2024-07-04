ac3ee645dca1974f70a4409bff8c76bd6355740e
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-07-05 01:18:35 +0200

    feat: extended grammar fuzzer und runner from sl with grammar for datetime (except leap year) and tested it

--- sl_implementation/GrammarFuzzer.py
+++ sl_implementation/GrammarFuzzer.py
@@ -26,6 +26,26 @@ UFUNC_GRAMMAR: Grammar = {
         ["np.array([" + ", ".join([str(random.uniform(-100.0, 100.0)) for _ in range(random.randint(1, 10))]) + "])" for _ in range(100)],
 }
 
+DATETIME_GRAMMAR: Grammar = {
+    "<start>": ["<function>"],
+    "<function>":
+            ["<date> - <date>", "<date> - <timespan>", "<timespan> / <timespan>", "np.busday_offset(<date>, <number>)", "np.is_busday(<date>)", "np.busday_count(<date>, <date>)"],
+    "<date>": ["np.datetime64('<formated_date>')"],
+    "<timespan>": ["np.timedelta64(<formated_timespan>)"],
+    "<formated_date>": ["<year>-<monthday>"],
+    "<formated_timespan>": ["NAT", "<number>, '<interval>'"],
+    "<year>": [str(i) for i in range(-1, 3000)],
+    "<monthday>": ["<month28>-<days28>", "<month30>-<days30>", "<month31>-<days31>"],
+    "<month28>": ["02"],
+    "<month30>": ["04", "06", "07", "09", "11"],
+    "<month31>": ["01", "03", "05", "08", "10", "12"],
+    "<days28>": ["0" + str(i) for i in range(1, 9)] + [str(i) for i in range(10, 28)],
+    "<days30>": ["0" + str(i) for i in range(1, 9)] + [str(i) for i in range(10, 30)],
+    "<days31>": ["0" + str(i) for i in range(1, 9)] + [str(i) for i in range(10, 31)],
+    "<number>": [str(i) for i in range(-100, 100)],
+    "<interval>": ["D", "M", "Y", "h", "m", "s"],
+}
+
 class GrammarFuzzer():
     def __init__(self, grammar: Grammar) -> None:
         self.grammar = grammar

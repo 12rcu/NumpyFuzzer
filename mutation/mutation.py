7896b9e85d8d4788f61bd7755fa997027847cfc0
Clemens <mail@clemens.jann.xyz>
2024-07-05 08:34:30 +0200

    feat: add datetime fuzzing

--- mutation/mutation.py
+++ mutation/mutation.py
@@ -5,7 +5,8 @@ from numpy.char import *
 from fuzzingbook.MutationFuzzer import *
 from fuzzingbook import Fuzzer
 from fuzzingbook.Fuzzer import *
-
+from datetime import datetime
+from dateutil import parser
 
 # Fuzzing book uses three methods: insert, delete and flip random character
 
@@ -23,7 +24,37 @@ def run():
         if reference_check != (True if outcome == "PASS" else False):
             print(str(i) + ": " + str(input_string) + ": " + str(outcome) + ". Reference says: " + str(reference_check) + ". Bytes: " + str(input_string.encode()))
 
+def run_date():
+    dates = ['2023-01-01', '2016-12-31 23:59:60.450', '1970-01-01 00:00:00', '0001-01-01 00:00:00', '9999-12-31 23:59:59', '1970-01-01 00:00:00']
+    mutation_fuzzer = MutationFuzzer(seed=dates)
+    runner = FunctionRunner(is_date_test)
 
+    for i in range(1000000000):
+        input_string = mutation_fuzzer.fuzz()
+
+        result, outcome = runner.run(input_string)
+
+        if outcome != "PASS":
+            np_date = np.datetime64(input_string)
+            print(str(i) + ": " + str(input_string) + ": " + str(np_date) + ". Reference says: " + str(datetime.strptime(input_string, '%m/%d/%y %H:%M:%S')))
+
+
+def is_date_test(input_string):
+    exception = False
+    np_date = None
+    py_date = None
+
+    try:
+        np_date = np.datetime64(input_string)
+    except Exception:
+        exception = True
+
+    try:
+        py_date = datetime.strptime(input_string, '%m/%d/%y %H:%M:%S')
+    except Exception:
+        return exception
+
+    return np_date.astype('datetime64[s]') == np.datetime64(py_date)
 
 def is_digit_test(input_string):
     if np.all(strings.isdigit(input_string)):
@@ -33,4 +64,6 @@ def is_digit_test(input_string):
 
 
 if __name__ == "__main__":
-    run()
+    run_date()
+
+# Not passing: 9/12/33 23:59:59
